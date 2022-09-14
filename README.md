1e6c38de34b807ed75e86ce532c13dd719355670
Sebastian Raschka <mail@sebastianraschka.com>
2022-09-14 16:50:42 -0500

    add hotspotter

--- README.md
+++ README.md
@@ -22,13 +22,76 @@ conda config --add channels conda-forge
 
 
 
-## Hotspotter script
+## Use the Hotspotter Script
 
 
 
 
 
-## Experiments
+Get usage information by running `python hotspotter.py --help`:
+
+
+
+```
+$ cd hotspotter
+$ python hotspotter.py --help
+[OK] Your Python version is 3.9.13 | packaged by conda-forge | (main, May 27 2022, 17:01:00) 
+[Clang 13.0.1 ]
+[OK] sklearn 1.1.2
+[OK] numpy 1.23.3
+[OK] joblib 1.1.0
+[OK] pandas 1.4.4
+usage: hotspotter.py [-h] --csv_path CSV_PATH [--trained_model TRAINED_MODEL]
+                     [--trained_model_sequence_only TRAINED_MODEL_SEQUENCE_ONLY] [--sequence-only {true,false}]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --csv_path CSV_PATH   Path to the CSV file
+  --trained_model TRAINED_MODEL
+                        Path to the trained model
+  --trained_model_sequence_only TRAINED_MODEL_SEQUENCE_ONLY
+                        Path to the trained model for --sequence_only prediction.
+  --sequence_only {true,false}
+  --print_row_numbers {true,false}
+```
+
+
+
+You CSV file should contain the same columns as the [[hotspotter/TestDataset.csv](hotspotter/TestDataset.csv)] file. If you are running the hotspotter in `--sequence-only true` mode, your CSV file does not have to contain the `secondary structure` column.
+
+If your CSV file contains additional columns, it doesn't matter. Also, the column order does not matter.
+
+If the conditions above are met, you can run the hotspotter as follows
+
+```
+python hotspotter.py --csv_path TestDataset.csv
+```
+
+(Note that `TestDataset.csv` is an example dataset we provide.)
+
+
+
+The results will be formatted as follows:
+
+```python hotspotter.py --csv_path TestDataset.csv 
+==========
+RESULTS
+==========
+1
+1
+1
+0
+0
+...
+1
+
+```
+
+Where each line corresponds to one row in the CSV file (1=hotspot, 0=not hotspot).
+
+
+
+## Use the Experiments Notebooks
 
 
 
@@ -36,7 +99,7 @@ conda config --add channels conda-forge
 
 
 
-conda install scikit-learn==1.1.2 jupyterlab watermark==2.3.1 matplotlib==3.5.3 pandas==1.4.4 mlxtend==0.21.0 --yes
+conda install scikit-learn==1.1.2 jupyterlab watermark==2.3.1 matplotlib==3.5.3 numpy==1.23.3 pandas==1.4.4 mlxtend==0.21.0 --yes
 
 
 
---
+++ hotspotter/hotspotter.py
@@ -0,0 +1,148 @@
+# Hotspotter script by Sebastian Raschka, 2022
+# More details in GitHub repository: https://github.com/Raschka-research-group/hotspotter
+
+import sys
+import pandas as pd
+import numpy as np
+from joblib import load
+from packaging import version
+import argparse
+from sklearn.preprocessing import OneHotEncoder
+
+
+def get_packages(pkgs):
+    versions = []
+    for p in pkgs:
+        try:
+            imported = __import__(p)
+            try:
+                versions.append(imported.__version__)
+            except AttributeError:
+                try:
+                    versions.append(imported.version)
+                except AttributeError:
+                    try:
+                        versions.append(imported.version_info)
+                    except AttributeError:
+                        versions.append('0.0')
+        except ImportError:
+            print(f'[FAIL]: {p} is not installed and/or cannot be imported.')
+            versions.append('N/A')
+    return versions
+
+
+def check_packages(d):
+
+    versions = get_packages(d.keys())
+
+    for (pkg_name, suggested_ver), actual_ver in zip(d.items(), versions):
+        if actual_ver == 'N/A':
+            continue
+        actual_ver, suggested_ver = version.parse(actual_ver), version.parse(suggested_ver)
+        if actual_ver < suggested_ver:
+            print(f'[FAIL] {pkg_name} {actual_ver}, please upgrade to >= {suggested_ver}')
+        else:
+            print(f'[OK] {pkg_name} {actual_ver}')
+
+
+if __name__ == "__main__":
+
+    if version.parse(sys.version.split(' ')[0].strip()) < version.parse('3.8'):
+        print('[FAIL] We recommend Python 3.8 or newer but'
+              ' found version %s' % (sys.version))
+    else:
+        print('[OK] Your Python version is %s' % (sys.version))
+
+    package_d = {
+        'sklearn': '1.1.2',
+        'numpy': '1.23.3',
+        'joblib': '1.1.0',
+        'pandas': '1.4.4'
+    }
+    check_packages(package_d)
+
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--csv_path", help="Path to the CSV file",
+                        type=str, required=True)
+
+    parser.add_argument("--trained_model", help="Path to the trained model",
+                        type=str, default="./mlp.joblib")
+
+    parser.add_argument("--trained_model_sequence_only",
+                        help="Path to the trained model for --sequence_only prediction.",
+                        type=str, default="./mlp_sequence_only.joblib")
+
+    parser.add_argument('--sequence_only',
+                        type=str,
+                        choices=("true", "false"),
+                        default="false")
+
+    parser.add_argument('--print_row_numbers',
+                        type=str,
+                        choices=("true", "false"),
+                        default="false")
+
+    args = parser.parse_args()
+
+    args_d = {'true': True,
+              'false': False}
+
+    args.sequence_only = args_d[args.sequence_only]
+    args.print_row_numbers = args_d[args.print_row_numbers]
+
+    df = pd.read_csv(args.csv_path)
+
+    feature_list = ['avg bond number', 'Hbond', 'residue',
+                    'Hphob', 'consurf', "B' side chain", 'asa']
+
+    if not args.sequence_only:
+        feature_list.append('secondary structure')
+
+    for feature in feature_list:
+        if feature not in df.columns:
+            raise ValueError(f"Did not find column {feature}")
+
+    df = df[feature_list]
+
+    # One-hot encoding
+    ohe = OneHotEncoder(drop='first')
+
+    # 1) convert aa char to int
+    codes = ['A', 'R', 'N', 'D', 'C', 'E', 'Q', 'G', 'H',
+             'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V']
+    code_to_int = {c:i for i,c in enumerate(codes)}     
+    df['residue'] = df['residue'].map(code_to_int)
+    df['residue'] = df['residue'].astype('category')
+
+    if args.sequence_only:
+        ohe_list = ['residue']
+    else:
+        codes = ['H', 'S', 'T', '-']
+        code_to_int = {c:i for i,c in enumerate(codes)}     
+        df['secondary structure'] = df['secondary structure'].map(code_to_int)
+        df['secondary structure'] = df['secondary structure'].astype('category')
+        ohe_list = ['residue', 'secondary structure']
+
+    ohe.fit(df[ohe_list])
+    df_ohe = df.drop(columns=ohe_list)
+    X_ohe_temp = np.asarray(ohe.transform(df[ohe_list]).todense())
+    X_ohe = np.hstack((df_ohe.values, X_ohe_temp))
+
+    if args.sequence_only:
+        pipe = load(args.trained_model_sequence_only)
+    else:
+        pipe = load(args.trained_model)
+
+    results = pipe.predict(X_ohe)
+
+    print('\n==========\nRESULTS\n==========')
+
+    if args.print_row_numbers:
+        for idx, i in enumerate(results):
+            print(f'{idx:04d}: {i}')
+
+    else:
+        for i in results:
+            print(i)
